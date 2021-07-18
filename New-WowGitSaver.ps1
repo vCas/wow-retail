09ac506cf4186a4fe996e16386710d6d312e8683
Viktor Bergqvist <bergqvist@kureq.com>
2021-07-18 23:12:32 +0200

    2021-07-18

--- New-WowGitSaver.ps1
+++ New-WowGitSaver.ps1
@@ -2,13 +2,24 @@
 
 $accounts = @("CASTHEWARLORD")
 $git = "git@github.com:vCas/wow-retail.git"
+$branch = "main"
 
+$includeFolders = @(
+    "!/Screenshots/**",
+    "!/Interface/AddOns/SharedMedia/**",
+    "!/Interface/AddOns/SharedMedia_MyMedia/**",
+    "!/Interface/AddOns/AKureqUI/**",
+    "!/Interface/ole/**",
+    "!/Interface/Textures/**",
+    "!/Interface/wizard/**"
+)
 $ignore = @"
 #Ignore All Folders?
 *
 !*/
 !.gitignore
 !New-WowGitSaver.ps1
+
 #CurseBreaker
 !/WTF/Config.wtf
 !/WTF/CurseBreaker.cache
@@ -19,35 +30,41 @@ $ignore = @"
 #Lol storing binary in git
 !CurseBreaker.exe
 
-#Interface Stuff
-!/Screenshots/**
-!/Interface/AddOns/SharedMedia/**
-!/Interface/AddOns/SharedMedia_MyMedia/**
-!/Interface/AddOns/AKureqUI/**
-!/Interface/ole/**
-!/Interface/Textures/**
-!/Interface/wizard/**
+"@
+
+$readme = @"
+
 "@
 
 $date = Get-Date -Format "yyyy-MM-dd"
 
 New-Item -Path . -Name ".gitignore" -ItemType "File" -Value $ignore -Force | Out-Null
+Add-Content -Path ".gitignore" -Value "`n#ACCOUNTS"
 foreach ($a in $accounts) {
+    Add-Content -Path ".gitignore" -Value "`n$a"
+}
+
+Add-Content -Path ".gitignore" -Value "`n#EXTRA FOLDERS"
+foreach ($f in $includeFolders) {
     Add-Content -Path ".gitignore" -Value "`n!/WTF/Account/$a/**"
+}
+
+if (-Not (Test-Path -Path "README.md")) {
+    New-Item -Path . -Name "README.md" -ItemType "File" -Value $readme -Force | Out-Null
 }
 
 if (-Not (Test-Path -Path ".git")) {
     git init
     git remote add origin $git
-    git branch -M main
+    git branch -M $branch
     git add .
     git commit -m"Save my wow"
-    git push -u origin main
+    git push -u origin $branch
 } elseif (Test-Path -Path ".git") {
 
     git add .
     git commit -m "$date"
-    git push -u origin main
+    git push -u origin $branch
 }
 
 if ($RestoreAddons) {
